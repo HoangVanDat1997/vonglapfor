6d3937ada7edf66ff0e3a4b2f4f86ac5f48fa934
hoangvandat <taiphiets666@gmail.com>
2022-09-13 16:29:23 +0700

    Vonglap

---
+++ lap.js
@@ -0,0 +1,18 @@
+function cc() {
+    var Dat = "";
+    var a = 1 ;
+    do {
+        Dat += "Số" + a + "<br>";
+        a++ ; console.log(Dat) ; {
+            if (Dat >10) {
+                alert("ĐÚng")
+            } else {
+                alert("Sai")
+            }
+        }
+
+
+    }
+    while (a<10)
+    document.write(Dat)
+}
